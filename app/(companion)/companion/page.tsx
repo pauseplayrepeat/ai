85fb4f99cb29e8f08451b2d360b1665746b38bff
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-08-26 21:32:02 -0500

    build companion component and displayed it on companion page

--- app/(companion)/companion/page.tsx
+++ app/(companion)/companion/page.tsx
@@ -1,8 +1,40 @@
 import { SearchInput } from '@/components/companion/search-input'
 import prismadb from '@/lib/prismadb'
 import Category from '@/components/companion/category'
+import { Companions } from '@/components/companion/companions'
 
-const CompanionPage = async () => {
+interface CompanionPageProps {
+  searchParams: {
+      categoryId: string;
+      name: string;
+  }
+} 
+
+const CompanionPage = async ({
+  searchParams
+}: CompanionPageProps) => {
+
+  const data = await prismadb.companion.findMany({
+    where: {
+      category: {
+        id: searchParams.categoryId,
+      },
+      name: {
+        search: searchParams.name
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    }
+  })
+
   const categories = await prismadb.category.findMany()
   
 
@@ -10,6 +42,7 @@ const CompanionPage = async () => {
     <div className="h-full p-4 space-y-2">
       <SearchInput />
       <Category data={categories} />
+      <Companions data={data}/>
     </div>
   )
 }
