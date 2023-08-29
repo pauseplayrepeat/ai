0794b924b501ee44aa69472e668055332bb88b1f
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-08-28 21:33:45 -0500

    updated video and music api route

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -33,10 +33,10 @@ export async function POST(
     // }
 
     const response = await replicate.run(
-      "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
       {
         input: {
-          prompt,
+          prompt_a: prompt
         }
       }
     );
@@ -47,7 +47,7 @@ export async function POST(
 
     return NextResponse.json(response);
   } catch (error) {
-    console.log('[VIDEO_ERROR]', error);
+    console.log('[MUSIC_ERROR]', error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 };
