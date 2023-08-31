d514154b0d002a3d95c955a6931fca9852165dd5
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-08-30 20:47:14 -0500

    no more paypal

--- components/companion/pro-modal.tsx
+++ components/companion/pro-modal.tsx
@@ -24,7 +24,7 @@ export const ProModal = () => {
         try {
             setLoading(true);
 
-            const response = await axios.get("api/stripe");
+            const response = await axios.get("/api/stripe");
 
             window.location.href = response.data.url;
 
