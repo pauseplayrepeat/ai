efaf4cc04c447c2433aabeedc925cdef3d11d3e9
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-08-30 20:12:31 -0500

    updated settings page

--- app/(companion)/companion/settings/page.tsx
+++ app/(companion)/companion/settings/page.tsx
@@ -2,19 +2,18 @@ import { checkSubscription } from "@/lib/subscriptions";
 import prismadb from "@/lib/prismadb";
 import { SubscriptionButton } from "@/components/companion/subscription-button";
 
-export const SettingsPage = async () => {
-
+const SettingsPage = async () => {
     const isPro = await checkSubscription();
-
+  
     return ( 
-        <div className="h-full p-4 space-y-2">
-            <h3 className="text-lg font-medium">Settings</h3>
-            <div className="text-muted-foreground text-sm">
-                {isPro ? "You are currently on the Pro plan" : "You are currently on the Free plan"}
-            </div>
-            <SubscriptionButton isPro={isPro} />
+      <div className="h-full p-4 space-y-2">
+        <h3 className="text-lg font-medium">Settings</h3>
+        <div className="text-muted-foreground text-sm">
+          {isPro ? "You are currently on a Pro plan." : "You are currently on a free plan."}
         </div>
-    );
-}
-
-export default SettingsPage;
+        <SubscriptionButton isPro={isPro} />
+      </div>
+     );
+  }
+   
+  export default SettingsPage;
