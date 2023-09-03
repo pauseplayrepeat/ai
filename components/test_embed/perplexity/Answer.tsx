a1acdf879e361fb9846ce1154394010a36e733d0
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-09-03 09:49:09 -0500

    update embed pages

--- components/test_embed/perplexity/Answer.tsx
+++ components/test_embed/perplexity/Answer.tsx
@@ -6,151 +6,151 @@ import * as React from "react"
 // import { CodeBlock } from '@/components/ui/codeblock'
 
 
-import { cn } from "@/lib/utils"
+// import { cn } from "@/lib/utils"
 // import {MemoizedReactMarkdown} from "@/components/MarkdownRenderer"
 // import { FadeIn } from "@/components/animations/FadeIn"
 // import { ResizablePanel } from "@/components/animations/ResizablePanel"
 // import { Icons } from "@/components/icons"
 // import { Sources } from "@/components/perplexity/Sources"
 
-export function AnswerCard({ question, answer, isCurrentAnswer, status }) {
-  return (
-    <div className=" py-10">
-      <div
-        className={cn(
-          "border-neutral-focus   rounded-xl border bg-neutral-100/50 p-4 shadow-md backdrop-blur ",
-          "hover:border-accent-focus text-left transition",
-          "dark:border-black/30 dark:bg-black/50",
+// export function AnswerCard({ question, answer, isCurrentAnswer, status }) {
+//   return (
+//     <div className=" py-10">
+//       <div
+//         className={cn(
+//           "border-neutral-focus   rounded-xl border bg-neutral-100/50 p-4 shadow-md backdrop-blur ",
+//           "hover:border-accent-focus text-left transition",
+//           "dark:border-black/30 dark:bg-black/50",
 
-          isCurrentAnswer
-            ? "border border-blue-200 ring-1 ring-inset transition duration-150 dark:border-blue-400/80 dark:ring-blue-500/40 "
-            : ""
-        )}
-      >
-        <ResizablePanel content={answer.message}>
-          <AnswerMessage
-            isCurrentAnswer={isCurrentAnswer}
-            content={answer.message}
-            error={status === "error"}
-            submittedQ={question}
-          />
+//           isCurrentAnswer
+//             ? "border border-blue-200 ring-1 ring-inset transition duration-150 dark:border-blue-400/80 dark:ring-blue-500/40 "
+//             : ""
+//         )}
+//       >
+//         <ResizablePanel content={answer.message}>
+//           <AnswerMessage
+//             isCurrentAnswer={isCurrentAnswer}
+//             content={answer.message}
+//             error={status === "error"}
+//             submittedQ={question}
+//           />
 
           {/* <AnimatePresence>
             {answer.sourceDocs ? <Sources sources={answer.sourceDocs} /> : null}
           </AnimatePresence> */}
-        </ResizablePanel>
-      </div>
-    </div>
-  )
-}
+  //       </ResizablePanel>
+  //     </div>
+  //   </div>
+  // )
+// }
 
-export function AnswerMessage({ submittedQ, isCurrentAnswer, content, error }) {
-  return (
-    <>
-      <AnimatedQuestion submittedQ={submittedQ} />
-      <div className="w-full py-2">
-        <div className="flex items-center">
-          <Icons.arrowDR className="h-6 w-6 text-blue-600 dark:text-blue-400" />
-          <p className="font-aboreto text-sm font-bold leading-tight tracking-wide text-blue-600 dark:text-blue-400">
-            PausePlayRepeat
-          </p>
-        </div>
-        <div className="w-full ">
-          {error || !content ? (
-            <LoadingLine />
-          ) : (
+// export function AnswerMessage({ submittedQ, isCurrentAnswer, content, error }) {
+//   return (
+//     <>
+//       <AnimatedQuestion submittedQ={submittedQ} />
+//       <div className="w-full py-2">
+//         <div className="flex items-center">
+//           <Icons.arrowDR className="h-6 w-6 text-blue-600 dark:text-blue-400" />
+//           <p className="font-aboreto text-sm font-bold leading-tight tracking-wide text-blue-600 dark:text-blue-400">
+//             PausePlayRepeat
+//           </p>
+//         </div>
+//         <div className="w-full ">
+//           {error || !content ? (
+//             <LoadingLine />
+//           ) : (
             // <MarkdownRenderer
             //   isCurrent={isCurrentAnswer}
             //   content={content}
             //   className=" min-w-full"
             // />
 
-            <div className="min-w-full pb-8">
-            <MemoizedReactMarkdown
-            className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 min-w-full"
-            // remarkPlugins={[remarkGfm, remarkMath]}
-            components={{
-                p({ children }: { children: React.ReactNode }) {
-                  return <p className="mb-2 last:mb-0">{children}</p>
-                },
-              code({ node, inline, className, children, ...props }) {
-                if (children.length) {
-                  if (children[0] == '▍') {
-                    return (
-                      <span className="mt-1 animate-pulse cursor-default">▍</span>
-                    )
-                  }
+//             <div className="min-w-full pb-8">
+//             <MemoizedReactMarkdown
+//             className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 min-w-full"
+//             // remarkPlugins={[remarkGfm, remarkMath]}
+//             components={{
+//                 p({ children }: { children: React.ReactNode }) {
+//                   return <p className="mb-2 last:mb-0">{children}</p>
+//                 },
+//               code({ node, inline, className, children, ...props }) {
+//                 if (children.length) {
+//                   if (children[0] == '▍') {
+//                     return (
+//                       <span className="mt-1 animate-pulse cursor-default">▍</span>
+//                     )
+//                   }
 
-                  children[0] = (children[0] as string).replace('`▍`', '▍')
-                }
+//                   children[0] = (children[0] as string).replace('`▍`', '▍')
+//                 }
 
-                const match = /language-(\w+)/.exec(className || '')
+//                 const match = /language-(\w+)/.exec(className || '')
 
-                if (inline) {
-                  return (
-                    <code className={className} {...props}>
-                      {children}
-                    </code>
-                  )
-                }
+//                 if (inline) {
+//                   return (
+//                     <code className={className} {...props}>
+//                       {children}
+//                     </code>
+//                   )
+//                 }
 
-                return (
-                  <CodeBlock
-                    key={Math.random()}
-                    language={(match && match[1]) || ''}
-                    value={String(children).replace(/\n$/, '')}
-                    {...props}
-                  />
-                )
-              }
-            }}
-          >
+//                 return (
+//                   <CodeBlock
+//                     key={Math.random()}
+//                     language={(match && match[1]) || ''}
+//                     value={String(children).replace(/\n$/, '')}
+//                     {...props}
+//                   />
+//                 )
+//               }
+//             }}
+//           >
 
 
-            {content}
+//             {content}
 
-          </MemoizedReactMarkdown>
-          </div>
-          )}
-        </div>
-      </div>
-    </>
-  )
-}
+//           </MemoizedReactMarkdown>
+//           </div>
+//           )}
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
 
-function AnimatedQuestion({ submittedQ }) {
-  return (
-    <AnimatePresence>
-      {submittedQ && (
-        <motion.h2
-          key={submittedQ}
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-          variants={fadeIn}
-          className="mx-auto text-2xl font-bold tracking-tighter"
-        >
-          {submittedQ}
-        </motion.h2>
-      )}
-    </AnimatePresence>
-  )
-}
+// function AnimatedQuestion({ submittedQ }) {
+//   return (
+//     <AnimatePresence>
+//       {submittedQ && (
+//         <motion.h2
+//           key={submittedQ}
+//           initial="hidden"
+//           animate="visible"
+//           exit="exit"
+//           variants={fadeIn}
+//           className="mx-auto text-2xl font-bold tracking-tighter"
+//         >
+//           {submittedQ}
+//         </motion.h2>
+//       )}
+//     </AnimatePresence>
+//   )
+// }
 
-function LoadingLine() {
-  return (
-    <div className="flex min-w-full animate-pulse px-4 py-5 sm:px-6">
-      <div className="flex grow space-x-3">
-        <div className="min-w-0 flex-1">
-          <div className="space-y-4 pt-4">
-            <div className="grid grid-cols-3 gap-4">
-              <div className="col-span-2 h-2 rounded bg-mauve-4"></div>
-              <div className="col-span-1 h-2 rounded bg-mauve-4"></div>
-            </div>
-            <div className="h-2 rounded bg-mauve-4"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+// function LoadingLine() {
+//   return (
+//     <div className="flex min-w-full animate-pulse px-4 py-5 sm:px-6">
+//       <div className="flex grow space-x-3">
+//         <div className="min-w-0 flex-1">
+//           <div className="space-y-4 pt-4">
+//             <div className="grid grid-cols-3 gap-4">
+//               <div className="col-span-2 h-2 rounded bg-mauve-4"></div>
+//               <div className="col-span-1 h-2 rounded bg-mauve-4"></div>
+//             </div>
+//             <div className="h-2 rounded bg-mauve-4"></div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
